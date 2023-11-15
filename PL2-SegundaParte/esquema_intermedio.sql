dc47dae77402c2da25fa810c144c9c9157ae6cc1
cegurene <carlos.eguren@edu.uah.es>
2023-11-15 09:40:44 +0100

    Esquema intermedio sin tildes

--- PL2-SegundaParte/esquema_intermedio.sql
+++ PL2-SegundaParte/esquema_intermedio.sql
@@ -3,8 +3,8 @@ CREATE TABLE Actores (
 	anno_nacimiento text NOT NULL,
 	anno_muerte text,
 	papel text,
-	año_películas text,
-	título_películas text,
+	año_pelculas text,
+	título_pelculas text,
 	CONSTRAINT Actores_pk PRIMARY KEY (nombre,anno_nacimiento)
 
 );
@@ -13,8 +13,8 @@ CREATE TABLE Directores (
 	nombre text NOT NULL,
 	anno_nacimiento text NOT NULL,
 	anno_muerte text,
-	año_películas text,
-	título_películas text,
+	anno_películas text,
+	titulo_películas text,
 	CONSTRAINT Directores_pk PRIMARY KEY (nombre,anno_nacimiento)
 
 );
@@ -23,61 +23,61 @@ CREATE TABLE Guionistas (
 	nombre text NOT NULL,
 	anno_nacimiento text NOT NULL,
 	anno_muerte text,
-	año_películas text,
-	título_películas text,
+	anno_películas text,
+	titulo_películas text,
 	CONSTRAINT Guionistas_pk PRIMARY KEY (nombre,anno_nacimiento)
 
 );
 
-CREATE TABLE películas (
-	año text NOT NULL,
-	título text NOT NULL,
+CREATE TABLE peliculas (
+	anno text NOT NULL,
+	titulo text NOT NULL,
 	idioma text,
-	duración text,
-	calificación text,
+	duracion text,
+	calificacion text,
 	generos text,
-	CONSTRAINT películas_pk PRIMARY KEY (año,título)
+	CONSTRAINT peliculas_pk PRIMARY KEY (anno,titulo)
 
 );
 
-CREATE TABLE críticas (
-	nombre_crítico text NOT NULL,
-	puntuación text,
-	texto_crítica text,
+CREATE TABLE criticas (
+	nombre_critico text NOT NULL,
+	puntuacion text,
+	texto_critica text,
 	url text,
-	año_películas text,
-	título_películas text,
-	CONSTRAINT críticas_pk PRIMARY KEY (nombre_crítico)
+	año_peliculas text,
+	titulo_peliculas text,
+	CONSTRAINT criticas_pk PRIMARY KEY (nombre_critico)
 
 );
 
-CREATE TABLE carátulas (
-	nombre_carátula text NOT NULL,
+CREATE TABLE caratulas (
+	nombre_caratula text NOT NULL,
 	fecha text,
 	url text,
-	tamaño text,
-	año_películas text,
-	título_películas text,
-	CONSTRAINT carátulas_pk PRIMARY KEY (nombre_carátula)
+	tamanno text,
+	anno_peliculas text,
+	titulo_peliculas text,
+	CONSTRAINT caratulas_pk PRIMARY KEY (nombre_caratula)
 
 );
 
-ALTER TABLE críticas ADD CONSTRAINT películas_fk FOREIGN KEY (año_películas,título_películas)
-REFERENCES películas (año,título) MATCH FULL
+ALTER TABLE criticas ADD CONSTRAINT peliculas_fk FOREIGN KEY (anno_peliculas,titulo_peliculas)
+REFERENCES peliculas (anno,titulo) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE carátulas ADD CONSTRAINT películas_fk FOREIGN KEY (año_películas,título_películas)
-REFERENCES películas (año,título) MATCH FULL
+ALTER TABLE carátulas ADD CONSTRAINT peliculas_fk FOREIGN KEY (anno_peliculas,titulo_peliculas)
+REFERENCES peliculas (anno,titulo) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE Actores ADD CONSTRAINT películas_fk FOREIGN KEY (año_películas,título_películas)
-REFERENCES películas (año,título) MATCH FULL
+ALTER TABLE Actores ADD CONSTRAINT peliculas_fk FOREIGN KEY (anno_peliculas,titulo_peliculas)
+REFERENCES peliculas (anno,titulo) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE Directores ADD CONSTRAINT películas_fk FOREIGN KEY (año_películas,título_películas)
-REFERENCES películas (año,título) MATCH FULL
+ALTER TABLE Directores ADD CONSTRAINT peliculas_fk FOREIGN KEY (anno_peliculas,titulo_peliculas)
+REFERENCES peliculas (anno,titulo) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE Guionistas ADD CONSTRAINT películas_fk FOREIGN KEY (año_películas,título_películas)
-REFERENCES películas (año,título) MATCH FULL
+ALTER TABLE Guionistas ADD CONSTRAINT peliculas_fk FOREIGN KEY (anno_peliculas,titulo_peliculas)
+REFERENCES peliculas (anno,titulo) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
